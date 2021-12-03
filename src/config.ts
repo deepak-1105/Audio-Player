98314a92420966e8fb8e9cefee53057c9e3ebc62
Deepak Sharma <85298639+deepak-1105@users.noreply.github.com>
2021-12-03 15:51:46 +0530

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  slices: 7,
+  sliceColors: [0x00ff00, 0xff0000, 0xffff00, 0x00ffff, 0xff00ff, 0xaa9988, 0xffffff],
+  prizes: ['5 Points', '20 Points', '50 Points', '100 Points', '200 Points', '500 Points', '1000 Points'],
+  weights: [30, 200, 20, 12, 10, 5, 1],
+  duration: 5,
+  radius: 250
+};
